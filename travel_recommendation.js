cb0cb7b62c0f52c5b4e70ada49dad457d32fb8d1
rocio4679 <rocio4679@hotmail.com>
2026-05-07 20:45:11 -0400

    Continue Javascript

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,21 +1,58 @@
 const btnSearch = document.getElementById('search');
 
 function FetchTravelAPI() {
-    fetch('travel_recommendation_api.json')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data); 
-        })
-        .catch(error => {
-            console.error("Error: ", error);
-            console.log("An error occurred!");
-        })
+    const Keyword = document.getElementById('navbar').value;
+    var RetKeyWord; 
+
+    RetKeyWord = KeywordSearch(Keyword);    
+
+    if(RetKeyWord != "error") 
+    {
+        fetch('travel_recommendation_api.json')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.filter(item => item.Keyword)); 
+                console.log(data);
+                 //Sample continue here
+                 const filtered = data.filter(user => 
+  user.name.toLowerCase().includes(searchTerm.toLowerCase())
+);
+            })
+            .catch(error => {
+                console.error("Error: ", error);
+                console.log("An error occurred!");
+            })
+    }
+    else {
+        alert("Please select a Valid Keyword!");
+    }
 }
 
 btnSearch.addEventListener('click', FetchTravelAPI);
 
 function KeywordSearch(Keyword) {
+    var ReturnKeyword;
     const KeyText = Keyword.toLowerCase(); 
+
+    if(KeyText.substring(0, 5) === "beach")
+    {
+        ReturnKeyword = "beaches";
+    } else {
+        if(KeyText === "country" || KeyText === "countries")
+        {
+            ReturnKeyword = "countries";
+        }   else {
+                if(KeyText.substring(0, 6) === "temple")
+                {
+                    ReturnKeyword = "temples";
+                } else {
+                    ReturnKeyword = "error"; 
+                }
+            } 
+        }
+
+    console.log(ReturnKeyword);
+    return ReturnKeyword;  
 }
 
 function ClearResults() {
